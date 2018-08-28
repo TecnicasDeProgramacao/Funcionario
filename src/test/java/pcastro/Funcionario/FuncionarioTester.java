d463765050882cfb55af20276fc938b1c471ef5b
LUCAS RIBEIRO DE ARAUJO <16104052@pucrs.br>
2018-08-28 11:27:49 -0300

    fodasse

--- src/test/java/pcastro/Funcionario/FuncionarioTester.java
+++ src/test/java/pcastro/Funcionario/FuncionarioTester.java
@@ -1,6 +1,7 @@
 package pcastro.Funcionario;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import junitparams.JUnitParamsRunner;
@@ -9,10 +10,11 @@ import junitparams.Parameters;
 @RunWith (JUnitParamsRunner.class)
 public class FuncionarioTester {
 	@Test
-	@Parameters ({"5000.00, 4000.00"})//??
-	public void FuncionarioTest(double val, double resp) {
-		Funcionario f1 = new Funcionario(10, "Pedro", 5000.0, 2, true);
+	@Parameters ({"1,Joao,1000,0,false,955","1,Joao,2000,0,false,1910","1,Joao,2500,0,false,1000","1,Joao,1000,0,false,550",
+		"1,Joao,1000,0,false,550","1,Joao,1000,0,false,550","1,Joao,1000,0,false,550",})//??
+	public void FuncionarioTest(int mat, String nome, double base, int dep, boolean ins, double liq) {
+		Funcionario f1 = new Funcionario(mat, nome, base, dep, ins);
 		double observed = f1.getSalarioLiquido();
-		assertEquals(observed, resp);
+		assertEquals(liq, observed);
 	}
 }
