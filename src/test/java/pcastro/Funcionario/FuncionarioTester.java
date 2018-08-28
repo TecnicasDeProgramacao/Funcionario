24ecbf93240b063ae630c5957cf6d6fba1c43ab2
PedroCastro1 <42178820+PedroCastro1@users.noreply.github.com>
2018-08-28 00:28:06 -0300

    Update FuncionarioTester.java

--- src/test/java/pcastro/Funcionario/FuncionarioTester.java
+++ src/test/java/pcastro/Funcionario/FuncionarioTester.java
@@ -10,7 +10,6 @@ import junitparams.Parameters;
 public class FuncionarioTester {
 	@Test
 	@Parameters ({"5000.00, 4000.00"})//??
-	
 	public void FuncionarioTest(double val, double resp) {
 		Funcionario f1 = new Funcionario(10, "Pedro", 5000.0, 2, true);
 		double observed = f1.getSalarioLiquido();
