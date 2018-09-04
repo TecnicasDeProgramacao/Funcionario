59ca3b4f88f1de83adbffa93ff858e0fa7d9ca33
KIN MAX PIAMOLINI GUSMAO <16104046@pucrs.br>
2018-09-04 10:28:22 -0300

    casos de teste

--- src/test/java/pcastro/Funcionario/CadastroFuncTester.java
+++ src/test/java/pcastro/Funcionario/CadastroFuncTester.java
@@ -13,13 +13,35 @@ class CadastroFuncTester {
 	@BeforeEach
 	public void inicializa() {
 		c = new CadastroFuncionario();		
-		c.addFuncionario(new Funcionario(1, "Marcos", 2000, 0, false));
-		c.addFuncionario(new Funcionario(2, "Marco", 5000, 0, true));
+		//c.addFuncionario(new Funcionario(1, "Marcos", 2000, 0, false));
+		//c.addFuncionario(new Funcionario(2, "Marco", 5000, 0, true));
 	}
 	
 	@Test
 	void sizeList() {	
 		assertThat(c.getList(),hasSize(50));
+	}
+	
+	@Test
+	public void nomesNaoVazios(){
+		assertThat(c.getList(),hasItem(allOf(hasProperty("nome",notNullValue()))));
+	}
+
+	@Test
+	public void matriculaMaior99(){
+		assertThat(c.getList(),hasItem(allOf(hasProperty("matricula",greaterThan(99)))));
+	}
+
+	@Test
+	public void testSemFilhosComInsalubridade(){
+		assertThat(c.getList(),hasItem(allOf(hasProperty("insalubridade",is(true)),
+				                               hasProperty("nroDependentes",is(0)))));
+	}
+
+	@Test
+	public void testComFilhosSemInsalubridade(){
+		assertThat(c.getList(),hasItem(allOf(hasProperty("insalubridade",is(false)),
+				                               hasProperty("nroDependentes",greaterThan(0)))));
 	}
 
 }
